ddd3672cae8768351064acbd49f0e139314a29c0
Jarid Mitchell <mitchellja13@students.ecu.edu>
2018-04-19 14:06:20 -0400

    checkDate function added. Only makes sure date exist and is atleast in 2018-9999. I still need to make helper functions to make sure date is in the future (not a past date) after making sure the date exists.

--- CableParadiseParkWeb/ScheduleAhead.aspx
+++ CableParadiseParkWeb/ScheduleAhead.aspx
@@ -14,7 +14,7 @@
             <br />
             Last Name:   <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
             <br />
-            Pick a Date:   <asp:TextBox ID="TextBox3" runat="server" Width="17px"></asp:TextBox>
+            Pick a Date:   <asp:TextBox ID="TextBox3" runat="server" Width="17px" OnTextChanged="TextBox3_TextChanged"></asp:TextBox>
             /<asp:TextBox ID="TextBox5" runat="server" Width="16px"></asp:TextBox>
             /<asp:TextBox ID="TextBox6" runat="server" Width="50px"></asp:TextBox>
 &nbsp;(MM/DD/YYYY)<br />
@@ -45,7 +45,7 @@
             <br />
             <asp:Button ID="Button2" runat="server" Text="See Price" />
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <asp:Button ID="Button1" runat="server" Text="Submit" />
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
             <br />
             <br />
             <asp:HiddenField ID="HiddenField1" runat="server" />
--- CableParadiseParkWeb/ScheduleAhead.aspx.cs
+++ CableParadiseParkWeb/ScheduleAhead.aspx.cs
@@ -5,13 +5,63 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 namespace CableParadiseParkWeb
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        private bool checkDate()
+        {
+            try
+            {
+                int day, month, year, startYear, endYear;
+                bool isDateValid = false;
+                startYear = 2018; //earliest year valid
+                endYear = 9999; //latest year valid
+
+                day = Convert.ToInt32(TextBox3.Text);
+                month = Convert.ToInt32(TextBox5.Text);
+                year = Convert.ToInt32(TextBox6.Text);
+
+                if (year >= startYear && year <= endYear)
+                {
+                    if ((day >= 1 && day <= 31) && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12))
+                    {
+                        isDateValid = true;
+                    }
+                    else if ((day >= 1 && day <= 30) && (month == 4 || month == 6 || month == 9 || month == 11))
+                    {
+                        isDateValid = true;
+                    }
+                    else if ((day >= 1 && day <= 28) && (month == 2))
+                    {
+                        isDateValid = true;
+                    }
+                    else if (day == 29 && month == 2 && (year % 400 == 0 || year % 100 != 0))
+                    {
+                        isDateValid = true;
+                    }
+                }
+                return isDateValid;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(checkDate())
+            {
+                
+            }
+            
         }
     }
 }
